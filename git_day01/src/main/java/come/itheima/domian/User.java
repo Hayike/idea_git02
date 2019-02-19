6107fedc1b685763565407f6e4f573d92c894d86
itcast <bos@itcast.cn>
2019-02-19 21:00:24 +0800

    新建的user类，提交到本地仓库, 修改后提交到本地仓库

--- git_day01/src/main/java/come/itheima/domian/User.java
+++ git_day01/src/main/java/come/itheima/domian/User.java
@@ -5,4 +5,29 @@ public class User {
     private  String name;
     private  String id;
 
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
